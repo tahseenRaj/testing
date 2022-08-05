bab753a17daebd2f0da0cd180fbc5031adf90c1f
tahseenRaj <raj.tahseen@gmail.com>
2022-08-05 10:24:10 +0500

    testing12

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.indigo,
+        primarySwatch: Colors.brown,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
@@ -108,7 +108,7 @@ class _MyHomePageState extends State<MyHomePage> {
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        child: const Icon(Icons.add_box_rounded),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
